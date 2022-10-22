c0aec20a06c9a113abf4c089a83e8cf912de57c4
git-egi <m.ier@hotmail.com>
2022-10-22 16:02:45 +0300

    RSA Keygen Done!

--- Assignment_1/rsa.c
+++ Assignment_1/rsa.c
@@ -2,6 +2,7 @@
 #include <gmp.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 /*
 	Create true, false enumeration
@@ -24,107 +25,159 @@ mpz_t P;
 mpz_t N;
 mpz_t L;
 mpz_t E;
+mpz_t D;
 
 // Temp variables used for calculations
 mpz_t tmpA;
 mpz_t tmpB;
 
-
-// This is going to hold number 7919 (large prime I found on wikipedia)
-// It's going to be used to generate e
-mpz_t L_Prime;
-
-
-//
-mpz_t Smallest_prime;
+/*----------------------------------------------------------------------- 
+						RSA IMPLEMENTATIONS
+----------------------------------------------------------------------- */
 
 
-bool IS_PRIME(mpz_t input){
-	/* The documentation suggests reps between 15 and 50 */
+/*	CHECK IF A NUMBER IS PRIME USING GMP LIBRARY*/
+bool IS_PRIME(mpz_t a){
+	/* GMP DOCS RECOMMEND 15 TO 50 REPS */
 	int reps = 15;
-	if(mpz_probab_prime_p(input, reps) != 0)
+	if(mpz_probab_prime_p(a, reps) != 0)
 		return TRUE;
 	else
 		return FALSE;
 }
 
+/*	SAVES KEYS PRODUCED EARLIER TO THE APPROPRIATE FILES
+	PUBLIC KEY 	-> PUBLIC.KEY FILE (FORMAT IS N,D)
+	PRIVATE KEY	-> PRIVATE.KEY FILE (FORMAT IS N,E) */
+void SAVE_KEYS(){
+	char * public_key_fname = "public.key";
+	char * private_key_fname = "private.key";
 
-void CHOOSE_E(){
-	/*I chose e = 65537 (most common value for e) because it has 2 bits set and is efficient for modular exponentiation*/ 
+	FILE* fpub = fopen(public_key_fname, "w");
+	FILE* fpriv = fopen(private_key_fname, "w");
+
+	if(!(fpub && fpriv)){
+		printf("Key Files Error !");
+		exit(-1);
+	}
+
+	fseek(fpub, 0, SEEK_SET);
+	fseek(fpriv, 0, SEEK_SET);
+
+	unsigned long int n = mpz_get_ui(N), d = mpz_get_si(D), e = mpz_get_ui(E);
+
+	/* Write Public Key */
+	fwrite(&n, sizeof(unsigned long int), 1, fpub);
+	fwrite(&d, sizeof(unsigned long int), 1, fpub);
+
+	/* Write Private Key */
+	fwrite(&n, sizeof(unsigned long int), 1, fpriv);
+	fwrite(&e, sizeof(unsigned long int), 1, fpriv);
+
+	fclose(fpub);
+	fclose(fpriv);
+}
+
+void READ_KEYS(char* fname, unsigned long int* a, unsigned long int* b){
+	FILE* file = fopen(fname, "r");
+	
+	if(!(file)){
+		printf("_______________________________________\n");
+		printf("Fopen Error : key file doesn't exist\n");
+		printf("_______________________________________\n");
+		exit(-1);
+	}
+
+	fseek(file, 0, SEEK_SET);
+	fread(a, sizeof(unsigned long int), 1, file);
+	fread(b, sizeof(unsigned long int), 1, file);
+
+	fclose(file);
+}
+
+/*	THIS METHOD MAKES ALL THE NEEDED CALCULATIONS
+	IN ORDER TO PRODUCE KEYS USING THE GMP LIBRARY */
+void PRODUCE_KEYS(){
+	printf("_______________________________________\n");
+	printf("Generate Keys\n");
+	printf("_______________________________________\n");
+	/* GET Q & P*/
+	/* MAKE SURE THEY ARE PRIMES AND SET GMP VARIABLES */
+	unsigned long int input_Q, input_P;	
+	printf("Enter the prime number Q : ");
+	scanf("%lu", &input_Q);
+	mpz_set_ui(Q, input_Q);
+	while(!IS_PRIME(Q)){
+		printf("Given Q is not a prime number, please type a prime : ");
+		scanf("%lu", &input_Q);
+		mpz_set_ui(Q, input_Q);
+	}
+	printf("Enter the prime number P : ");
+	scanf("%lu", &input_P);
+	mpz_set_ui(P, input_P);
+	while(!IS_PRIME(P)){
+		printf("Given P is not a prime number, please type a prime : ");
+		scanf("%lu", &input_P);
+		mpz_set_ui(P, input_P);
+	}
+	/* SET GMP VARIABLES FOR Q-1 & P-1 */
+	mpz_set_ui(tmpA, input_Q-1);
+	mpz_set_ui(tmpB, input_P-1);
+	/* CALCULATE ( P * Q ) */
+	mpz_mul(N, P, Q);
+	/* CALCULATE LAMDA(N) */
+	mpz_mul(L, tmpA, tmpB);
+	/* I CHOSE E = 65537 (MOST COMMON VALUE FOR E) BECAUSE IT HAS 2 BITS SET AND IS IS EFFICIENT FOR MODULAR EXPONENTATION*/ 
+	/* I COULD HAVE CHOSE E = 3 BUT IT IS SHOWN TO BE LESS SECURE IN SOME SETTINGS*/
 	mpz_set_ui(E, 65537);
-	/* e % lambda(n) */
+	/* E MOD LAMDA(N) */
 	mpz_mod(tmpA, E, L);
-	/* gcd(e, lambda(n)) */
+	/* GCD( E, LAMDA(N) ) */
 	mpz_gcd(tmpB, E, L);
-
-	unsigned long int e, l, mod, gcd;
-
-	e = mpz_get_ui(E);
-	l = mpz_get_ui(L);
+	/* GET RESULTS OF MOD, GCD TO TEST */
+	unsigned long int mod, gcd;
 	mod = mpz_get_ui(tmpA);
 	gcd = mpz_get_ui(tmpB);
-	
-	printf("e : \t%lu\n",e);
-	printf("l : \t%lu\n",l);
-	printf("---------------------------------------\n");
-	printf("mod : \t%lu\t|\tPASS\n",mod);
-	printf("gcd : \t%lu\t|\tPASS\n",gcd);
-	printf("---------------------------------------\n");
+	/* CALCULATE MODULAR INVERSE OF E,L */
+	mpz_invert(D, E, L);
+	/* SETTING INFORMATION */
+	printf("_______________________________________\n");
+	printf("Settings\n");
+	printf("_______________________________________\n");
+	gmp_printf("Q =\t\t\t%Zd\nP =\t\t\t%Zd\n", Q, P);
+	gmp_printf("N =\t\t\t%Zd\n", N);
+	gmp_printf("lambda(N) =\t\t%Zd\n", L);
+	gmp_printf("e =\t\t\t%Zd\n",E);
+	printf("_______________________________________\n");
+	printf("E constaints\n");
+	printf("_______________________________________\n");
+	printf("Mod(e, lambda(N)) =\t%lu\n",mod);
+	printf("GCD(e, lambda(N)) =\t%lu\n",gcd);
+	printf("_______________________________________\n");
+	gmp_printf("D =\t\t\t%Zd\n", D);
+	printf("_______________________________________\n");
 
+	SAVE_KEYS();
 }
+
+
+void RSA_ENCRYPT(){
+	printf("_______________________________________\n");
+	printf("Encrypt\n");
+	printf("_______________________________________\n");
+	unsigned long int n,d;
+	/*Pull public keys from file*/
+	printf("KEY INPUT FILE = %s\n",KEY_FILE);
+	READ_KEYS(KEY_FILE, &n, &d);
+	printf("n = %lu, d = %lu\n",n,d);
+}
+
 
 void CHECK_ARGS(){
 	if(GENERATE_KEYS && !ENCRYPT && !DECRYPT && INPUT_FILE==NULL && OUTPUT_FILE==NULL && KEY_FILE ==NULL){
-		
-		unsigned long int input_Q, input_P;
-
-		printf("---------------------------------------\nGenerate Keys\n---------------------------------------\n");
-		
-		/* Set  Q & P */
-		
-		printf("Enter the prime number Q : ");
-		scanf("%lu", &input_Q);
-		mpz_set_ui(Q, input_Q);
-		while(!IS_PRIME(Q)){
-			printf("Given Q is not a prime number, please type a prime number : ");
-			scanf("%lu", &input_Q);
-			mpz_set_ui(Q, input_Q);
-		}
-
-		printf("Enter the prime number P : ");
-		scanf("%lu", &input_P);
-		mpz_set_ui(P, input_P);
-		while(!IS_PRIME(P)){
-			printf("Given P is not a prime number, please type a prime number : ");
-			scanf("%lu", &input_P);
-			mpz_set_ui(P, input_P);
-		}
-
-
-		/* Set Q - 1 & P - 1, so we can compute lambda(n)*/
-		mpz_set_ui(tmpA, input_Q-1);
-		mpz_set_ui(tmpB, input_P-1);
-
-
-		/* Calculate N  = Q * P */
-		mpz_mul(N, P, Q);
-		/* Calculate Lamda(n) */
-		mpz_mul(L, tmpA, tmpB);
-		
-		printf("---------------------------------------\n");
-		gmp_printf("Q = %Zd, P = %Zd\n", Q, P);
-		printf("---------------------------------------\n");
-		gmp_printf("N = %Zd\n", N);
-		printf("---------------------------------------\n");
-		gmp_printf("Lamda(N) = %Zd\n", L);
-		printf("---------------------------------------\n");
-
-
-		CHOOSE_E();
-
-
+		PRODUCE_KEYS();
 	}else if(ENCRYPT && !GENERATE_KEYS && !DECRYPT && INPUT_FILE != NULL && OUTPUT_FILE != NULL && KEY_FILE !=NULL){
-		printf("Encrypt\n");
+		RSA_ENCRYPT();
 	}else if(DECRYPT && !GENERATE_KEYS && !ENCRYPT && INPUT_FILE != NULL && OUTPUT_FILE != NULL && KEY_FILE !=NULL){
 		printf("Decrypt\n");
 	}else{
@@ -132,12 +185,16 @@ void CHECK_ARGS(){
 	}
 }
 
+
+
+/*----------------------------------------------------------------------- 
+								MAIN
+----------------------------------------------------------------------- */
+
 int main(int argc, char *argv[]){
-
-	mpz_inits(Q, P, N, L, E, tmpA, tmpB, L_Prime, NULL);
-
+	mpz_inits(Q, P, N, L, E, D, tmpA, tmpB, NULL);
 	int opt;
-	while((opt= getopt(argc, argv, "i:o:k:gdeh")) != -1){
+	while((opt= getopt(argc, argv, "i:o:k:gdehr")) != -1){
 		switch(opt){
 			case 'i':
 				INPUT_FILE = strdup(optarg);
@@ -163,7 +220,6 @@ int main(int argc, char *argv[]){
 				return 0;
 		}
 	}
-
 	CHECK_ARGS();
 }
 
