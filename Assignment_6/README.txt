5c6507e613bd4234b534682b1c6e787cd6807085
git-egi <git-egi>
2022-12-27 17:07:19 +0200

    Assign. 6 Final

--- Assignment_6/README.txt
+++ Assignment_6/README.txt
@@ -1,3 +1,40 @@
--------------------------------------------------------
-	Creating IPTable Rules From Given Domains
--------------------------------------------------------
+---------------------------------------------------------
+Emmanouil-Georgios Ieronymakis A.M. 2015030136
+---------------------------------------------------------
+Ubuntu 20.04.5 LTS
+---------------------------------------------------------
+Assignment 6
+---------------------------------------------------------
+In this Assignment I created a simple bash script which creates
+firewall rules from given domain names using iptables and other linux commands.
+---------------------------------------------------------
+Implementation
+---------------------------------------------------------
+- domains option
+---------------------------------------------------------
+In the first step I separated the common domains from the different domains, using 
+the grep command on the two domain files.
+
+For both different and common domains I used dig command to resolve the IP address
+of each domain.
+
+I filtered out CNAME's, A records etc. from the IP files and kept only IP Addresses (using grep with regex),
+that's because iptables expects IP's as source/destination arguments not domain names...
+---------------------------------------------------------
+Instruction file Questions
+---------------------------------------------------------
+1. After configuring the adblock rules test your script 
+by visiting your favourite websites without 
+any other adblocking mechanism (e.g., adblock browser extensions).
+Can you see ads? Do they load? Some ads persist, why?
+---------------------------------------------------------
+Answer
+---------------------------------------------------------
+I mostly used https://d3ward.github.io/toolz/adblock to test out my Implementation.
+I did manage to block most of the ads using domain names provided by this site,
+and I saw the number of dropped, reject packets and bytes for every rule 
+using the -list option.
+Some ads still persisted but that happens due to the fact dns lookup for those domains
+result to CNAME's, A records or nothing.
+---------------------------------------------------------
+
--- Assignment_6/adblock.sh
+++ Assignment_6/adblock.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # You are NOT allowed to change the files' names!
 domainNames="domainNames.txt"
 domainNames2="domainNames2.txt"
@@ -13,131 +14,93 @@ function adBlock() {
     fi
     if [ "$1" = "-domains"  ]; then
 
-        echo "Processing domain names..."
-
-        # Clear files from previous executions
-
+        # Remove temporary files used for processing
         rm -rf commonDomains.txt
         rm -rf differentDomains.txt
-        rm -rf $IPAddressesSame
-        rm -rf $IPAddressesDifferent
+        rm -rf tmpIPsSame.txt
+        rm -rf tmpIPsDiff.txt
+        rm -rf tmpIPsSameUniq.txt
+        rm -rf tmpIPsDiffUniq.txt
 
-        # Get lines in file2 not in file1
-        join -v 1 <(sort $domainNames2) <(sort $domainNames) >> differentDomains.txt
-        # Get lines in file1 not in file2
-        join -v 2 <(sort $domainNames2) <(sort $domainNames) >> differentDomains.txt
-        # Get common lines of file1 and file2
+        echo "Processing domain names, this might take some time..."
+
+        # grep options used : 
+        # -F, Interpret PATTERNS as fixed strings, not regular expressions
+        # -x, Select only those matches that exactly match the whole line
+        # -f, Obtain patterns from FILE, one per line.
+        # -v, Invert the sense of matching, to select non-matching lines.
+
+        # Get lines in domainNames not in domainNames2 (lines are domains in our case)
+        grep -Fxvf $domainNames $domainNames2 >> differentDomains.txt
+        # Get lines in domainNames2 not in domainNames (lines are domains in our case)
+        grep -Fxvf $domainNames2 $domainNames >> differentDomains.txt
+        # Get common lines (domains) of file1 and file2
         grep -Fxf $domainNames $domainNames2> commonDomains.txt
 
-        # Read common domains
-        while read domain
-        do
-        dig_ret=$(dig +short "$domain")
-        size=${#dig_ret} 
-        #if the IP Address is empty or if there is a timeout don't append to file
-        if [[ $dig_ret == *"timed out"* || $size == 0 ]] 
-        then 
-            continue
-        else
-            echo "${dig_ret}" >> IPAddressesSa.txt
-        fi
-        done < commonDomains.txt
+        # dig options used :
+        # -f, read from file
+        # +short, short answer
 
-        # Read different domains
-        while read domain
-        do
-        dig_ret=$(dig +short "$domain")
-        size=${#dig_ret} 
-        if [[ $dig_ret == *"timed out"* || $size == 0 ]] 
-        then 
-            continue
-        else
-            echo "${dig_ret}" >> IPAddressesDiff.txt
-        fi
-        done < differentDomains.txt
+        # perform DNS lookup for domains in domain files and export results to IP file
+        dig +short -f commonDomains.txt >> tmpIPsSame.txt
+        dig +short -f differentDomains.txt >> tmpIPsDiff.txt
 
-        # Keep the unique IP Addresses in each file
-        awk '{!seen[$0]++};END{for(i in seen) if(seen[i]>0)print i}' IPAddressesDiff.txt | sort >> IPAddressesDiffUniq.txt
-        awk '{!seen[$0]++};END{for(i in seen) if(seen[i]>0)print i}' IPAddressesSa.txt | sort >> IPAddressesSaUniq.txt
+        # awk loops through the lines and counts appearances then prints each unique,
+        # then sort printed and export to file
+        awk '{!seen[$0]++};END{for(i in seen) if(seen[i]>0)print i}' tmpIPsSame.txt | sort >> tmpIPsSameUniq.txt
+        awk '{!seen[$0]++};END{for(i in seen) if(seen[i]>0)print i}' tmpIPsDiff.txt | sort >> tmpIPsDiffUniq.txt
 
-
-        # Filter out everything that's not IP
-        grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' IPAddressesDiffUniq.txt >> $IPAddressesDifferent
-        grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' IPAddressesSaUniq.txt >> $IPAddressesSame
-        
-        # Remove files used for processing
-        rm -rf IPAddressesDiff.txt
-        rm -rf IPAddressesDiffUniq.txt
-        rm -rf IPAddressesSa.txt
-        rm -rf IPAddressesSaUniq.txt
-        rm -rf commonDomains.txt
-        rm -rf differentDomains.txt
-
-        echo "--------------------------------------------------------------------"
+        # Filter only IP's using grep and regex, then export them to file (filters out CNAME's, A records etc.)
+        # iptables' souce/destination address arg expects ip's not domain names...
+        grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' tmpIPsSameUniq.txt  >> $IPAddressesSame
+        grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' tmpIPsDiffUniq.txt >> $IPAddressesDifferent
+	
+	echo "--------------------------------------------------------------------"
         echo "Processing Completed! IP's were exported to the respective files..."
         echo "--------------------------------------------------------------------"
+
+        # Remove temporary files used for processing
+        rm -rf commonDomains.txt
+        rm -rf differentDomains.txt
+        rm -rf tmpIPsSame.txt
+        rm -rf tmpIPsDiff.txt
+        rm -rf tmpIPsSameUniq.txt
+        rm -rf tmpIPsDiffUniq.txt
 
         true
             
     elif [ "$1" = "-ipssame"  ]; then
-        # Configure the DROP adblock rule based on the IP addresses of $IPAddressesSame file.
-        # Write your code here...
-        # ...
-        # ...
-        while IFS= read line
+        # Set DROP rule for each IP in IPAddressesSame.txt
+        while IFS= read ip
         do
-            iptables -A INPUT -s $line -j DROP
+            iptables -A INPUT -s $ip -j DROP
 
         done < $IPAddressesSame
-
         true
     elif [ "$1" = "-ipsdiff"  ]; then
-        # Configure the REJECT adblock rule based on the IP addresses of $IPAddressesDifferent file.
-        # Write your code here...
-        # ...
-        # ...
-        while IFS= read line
+        # Set REJECT rule for each IP in IPAddressesDifferent.txt
+        while IFS= read ip
         do
-            iptables -A INPUT -s $line -j REJECT
+            iptables -A INPUT -s $ip -j REJECT
 
         done < $IPAddressesDifferent
-
         true
-        
     elif [ "$1" = "-save"  ]; then
-        # Save rules to $adblockRules file.
-        # Write your code here...
-        # ...
-        # ...
+        # Export adblock rules to adblockRules file
         iptables-save > $adblockRules
         true
-        
     elif [ "$1" = "-load"  ]; then
-        # Load rules from $adblockRules file.
-        # Write your code here...
-        # ...
-        # ...
+        # Load adblock rules from adblockRules file
         iptables-restore < $adblockRules
         true
-
-        
     elif [ "$1" = "-reset"  ]; then
-        # Reset rules to default settings (i.e. accept all).
-        # Write your code here...
-        # ...
-        # ...
+        # Reset IP tables rule set
         iptables -F
         true
-
-        
     elif [ "$1" = "-list"  ]; then
-        # List current rules.
-        # Write your code here...
-        # ...
-        # ...
+        # List IP tables rules
         iptables --list -n -v
         true
-        
     elif [ "$1" = "-help"  ]; then
         printf "This script is responsible for creating a simple adblock mechanism. It rejects connections from specific domain names or IP addresses using iptables.\n\n"
         printf "Usage: $0  [OPTION]\n\n"
